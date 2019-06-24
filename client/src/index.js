123a597197df69e1d833d1d0b5823b213cb34a87
afbernardino <andre.bernardino@tecnico.ulisboa.pt>
2019-06-24 19:29:02 +0100

    Finish changing project's structure

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import createStore from "./store/createStore";
-import AppRouter from "./components/AppRouter";
+import AppRouter from "./routers/AppRouter";
 
 const store = createStore();
 
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import UserRegister from "./users/UserRegister";
+import UserRegister from "../components/users/UserRegister";
 import { createBrowserHistory } from "history";
 
 export const history = createBrowserHistory();
